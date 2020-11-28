b82a35d250badb6c057aaed443cc9b7bbaa80525
Mohamad Ivan Fanany <fananymi@delligence.ai>
2020-11-28 13:48:52 +0700

    First commit

---
+++ pp204.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+docker run --rm -d \
+  --name jenkins \
+  -u root \
+  -p 8080:8080 \
+  -v jenkins-data:/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v "$HOME":/home \
+  jenkinsci/blueocean
+
+docker container exec jenkins cat /var/jenkins_home/secrets/initialAdminPassword
